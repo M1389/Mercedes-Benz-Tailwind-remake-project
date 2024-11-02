80868ed508e325a7ffe240c07f5db32897c89972
Mahdi Hashemi <95425009+M1389@users.noreply.github.com>
2024-11-01 17:15:24 -1200

    nav bar 90% completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 'use strict'
 
+// nav bar js starts here
+
+let openMenuBtn = document.querySelector('#openMenu');
+let responsiveNav = document.querySelector('#linksDiv');
+openMenuBtn.addEventListener('click',()=>{
+    responsiveNav.classList.toggle('min-[275px]:invisible');
+    responsiveNav.classList.toggle('min-[275px]:h-[100vh]');
+    if(responsiveNav.classList.contains('sm:invisible')){
+        openMenuBtn.textContent = 'Menu'
+    }else{
+        openMenuBtn.textContent = 'Close'
+    }
+})
 
 
+// nav bar js Ends here
