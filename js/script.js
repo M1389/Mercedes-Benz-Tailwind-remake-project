cb7ce13ed554a3121fec7c48a080dfbb1ee81e2c
Mahdi Hashemi <95425009+M1389@users.noreply.github.com>
2024-11-01 23:19:11 -1200

    end of hamburger menu and start search bar

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ let responsiveNav = document.querySelector('#linksDiv');
 openMenuBtn.addEventListener('click',()=>{
     responsiveNav.classList.toggle('min-[275px]:invisible');
     responsiveNav.classList.toggle('min-[275px]:h-[100vh]');
-    if(responsiveNav.classList.contains('sm:invisible')){
+    
+    if(responsiveNav.classList.contains('min-[275px]:invisible')){
         openMenuBtn.textContent = 'Menu'
     }else{
         openMenuBtn.textContent = 'Close'
